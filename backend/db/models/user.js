b8404b597dd37886dcc8aa77fcc2912395c78fdc
Max Yaswen <myaswen@gmail.com>
2022-09-26 13:52:22 -0700

    Add Spots model, migration, & seed

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -51,7 +51,10 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static associate(models) {
-      // define association here
+      User.hasMany(models.Spot, {
+        foreignKey: 'ownerId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   User.init({
