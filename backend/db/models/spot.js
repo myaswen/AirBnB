94675d818dccaade6e4938919d560931ac62d03d
Max Yaswen <myaswen@gmail.com>
2022-09-27 10:21:46 -0700

    Minor formatting edits

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -4,17 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Spot.belongsTo(models.User, {
         foreignKey: 'ownerId'
       });
       Spot.hasMany(models.Booking, {
-        foreignKey: 'spotId', // Updated--inform slack
+        foreignKey: 'spotId',
         onDelete: 'CASCADE'
       });
       Spot.hasMany(models.Review, {
