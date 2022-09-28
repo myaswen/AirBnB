fa77483d063834cc422e6dde7485fa09886a0682
Max Yaswen <myaswen@gmail.com>
2022-09-28 15:03:10 -0700

    Add Spot-Image router

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+const router = express.Router();
+
+
+
+module.exports = router;
