0f40f99d452e691c9e26596f782afc19263a4f00
Max Yaswen <myaswen@gmail.com>
2022-10-16 12:12:21 -0700

    Add body and footer components

---
+++ frontend/src/components/FooterContent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './FooterContent.css';
+
+const FooterContent = () => {
+    return (
+        <div className="footer_wrapper">
+            © 2022 Tbdbnb, Inc. · <a href="https://github.com/myaswen/AirBnB">GitHub</a>
+        </div>
+    );
+}
+
+export default FooterContent;
