93ba8455d8cee273aaffc5b2ef7c8119a23e6040
Max Yaswen <myaswen@gmail.com>
2022-10-16 15:18:49 -0700

    Add spot redux & refactor spot cards to live data

--- frontend/src/components/HomeBodyContent/index.js
+++ frontend/src/components/HomeBodyContent/index.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './HomeBodyContent.css';
 import SpotCard from "../SpotCard";
 
+import { useSelector, useDispatch } from "react-redux";
+import { TH_fetchSpots } from "../../store/spotReducer";
+
 const HomeBodyContent = () => {
+    const dispatch = useDispatch();
+
+    const spotsObject = useSelector(state => state.spots.allspots);
+    const spotIds = Object.keys(spotsObject);
+
+    useEffect(() => {
+        dispatch(TH_fetchSpots());
+    }, [dispatch]);
+
     return (
         <div className="body_wrapper">
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
-            <SpotCard />
+            {
+                spotIds.map(id => (
+                    <SpotCard
+                    spot={spotsObject[id]}
+                    key={id}
+                    />
+                ))
+            }
         </div>
     );
 }
--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -2,19 +2,35 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './SpotCard.css';
 
-const SpotCard = () => {
+const SpotCard = ({ spot }) => {
+    // return (
+    //     <Link to="" className="card_wrapper">
+    //         <img className="preview_image" src="https://a0.muscache.com/im/pictures/3213f86d-1c2b-4214-90fe-20d51dc8f182.jpg?im_w=960" alt='preview' />
+    //         <div className="location_rating">
+    //             <div>Sonoma, California</div>
+    //             <div>
+    //                 <i className="fa-solid fa-star"></i>
+    //                 4.95
+    //             </div>
+    //         </div>
+    //         <div className="price">
+    //             <span className="bold">$350</span> night
+    //         </div>
+    //     </Link>
+    // );
+
     return (
         <Link to="" className="card_wrapper">
-            <img className="preview_image" src="https://a0.muscache.com/im/pictures/3213f86d-1c2b-4214-90fe-20d51dc8f182.jpg?im_w=960" alt='preview' />
+            <img className="preview_image" src={spot.previewImage} alt='preview' />
             <div className="location_rating">
-                <div>Sonoma, California</div>
+                <div>{`${spot.city}, ${spot.state}`}</div>
                 <div>
                     <i className="fa-solid fa-star"></i>
-                    4.95
+                    {spot.avgRating}
                 </div>
             </div>
             <div className="price">
-                <span className="bold">$350</span> night
+                <span className="bold">${spot.price}</span> night
             </div>
         </Link>
     );
