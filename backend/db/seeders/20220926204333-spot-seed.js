054b5cdc13b2cd869d96fa67f0b8de192024ede1
Max Yaswen <myaswen@gmail.com>
2022-10-16 17:15:50 -0700

    Add additional spot & image seeds

--- backend/db/seeders/20220926204333-spot-seed.js
+++ backend/db/seeders/20220926204333-spot-seed.js
@@ -16,6 +16,7 @@ module.exports = {
     });
 
     return queryInterface.bulkInsert('Spots', [
+      // Spot 1:
       {
         ownerId: userOne.id,
         address: "1436 Scheuvront Drive",
@@ -28,6 +29,7 @@ module.exports = {
         description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
         price: 516,
       },
+      // Spot 2:
       {
         ownerId: userTwo.id,
         address: "4577 Charter Street",
@@ -40,6 +42,7 @@ module.exports = {
         description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
         price: 414,
       },
+      // Spot 3:
       {
         ownerId: userThree.id,
         address: "4786 Hiney Road",
@@ -51,6 +54,97 @@ module.exports = {
         name: "Cabin in the woods",
         description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
         price: 400,
+      },
+      // Spot 4:
+      {
+        ownerId: userThree.id,
+        address: "1114 Branch Road",
+        city: "Sea Ranch",
+        state: "California",
+        country: "United States of America",
+        lat: 38.7152,
+        lng: 123.4545,
+        name: "Industrial Design in Nature",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 364,
+      },
+      // Spot 5:
+      {
+        ownerId: userThree.id,
+        address: "4901 Point Street",
+        city: "Watsonville",
+        state: "California",
+        country: "United States of America",
+        lat: 36.9102,
+        lng: 121.7569,
+        name: "Ocean Front Views",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 980,
+      },
+      // Spot 6:
+      {
+        ownerId: userThree.id,
+        address: "762 Duck Drive",
+        city: "Santa Cruz",
+        state: "California",
+        country: "United States of America",
+        lat: 36.9741,
+        lng: 122.0308,
+        name: "Cozy By The Beach",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 300,
+      },
+      // Spot 7:
+      {
+        ownerId: userThree.id,
+        address: "2694 Parkway Street",
+        city: "Terlingua",
+        state: "Texas",
+        country: "United States of America",
+        lat: 29.3216,
+        lng: 103.6160,
+        name: "Adobe Dome",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 111,
+      },
+      // Spot 8:
+      {
+        ownerId: userThree.id,
+        address: "2567 Melody Lane",
+        city: "Williams",
+        state: "Arizona",
+        country: "United States of America",
+        lat: 35.2495,
+        lng: 112.1910,
+        name: "Tiny A-Frame Camping",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 199,
+      },
+      // Spot 9:
+      {
+        ownerId: userThree.id,
+        address: "3494 Boundary Street",
+        city: "Oakhurst",
+        state: "California",
+        country: "United States of America",
+        lat: 37.3280,
+        lng: 119.6493,
+        name: "Luxury Estate on a River",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 995,
+      },
+      // Spot 10:
+      {
+        ownerId: userThree.id,
+        address: "3008 Tetrick Road",
+        city: "Portland",
+        state: "Oregon",
+        country: "United States of America",
+        lat: 45.5152,
+        lng: 122.6784,
+        name: "Modern Tranquility",
+        description: "Relax and enjoy yourself at this unique stay. The home is architecturally inspired with fine details hidden in every corner. There's space to stretch out and disconnect from the world, or reconnect with family and friends. There is ample room for activities, inside and out, and lots of attractions nearby to explore if you feel adventurous. There's always something to do, or nothing to do, depending on your goal. I hope you enjoy your stay!",
+        price: 150,
       }
     ]);
   },
--- backend/db/seeders/20220927152638-spotImage-seed.js
+++ backend/db/seeders/20220927152638-spotImage-seed.js
@@ -6,13 +6,34 @@ module.exports = {
   async up(queryInterface, Sequelize) {
 
     const spotOne = await Spot.findOne({
-      where: { name: "A Frame Retreat" }
+      where: { address: "1436 Scheuvront Drive" }
     });
     const spotTwo = await Spot.findOne({
-      where: { name: "Mountain Retreat" }
+      where: { address: "4577 Charter Street" }
     });
     const spotThree = await Spot.findOne({
-      where: { name: "Cabin in the woods" }
+      where: { address: "4786 Hiney Road" }
+    });
+    const spotFour = await Spot.findOne({
+      where: { address: "1114 Branch Road" }
+    });
+    const spotFive = await Spot.findOne({
+      where: { address: "4901 Point Street" }
+    });
+    const spotSix = await Spot.findOne({
+      where: { address: "762 Duck Drive" }
+    });
+    const spotSeven = await Spot.findOne({
+      where: { address: "2694 Parkway Street" }
+    });
+    const spotEight = await Spot.findOne({
+      where: { address: "2567 Melody Lane" }
+    });
+    const spotNine = await Spot.findOne({
+      where: { address: "3494 Boundary Street" }
+    });
+    const spotTen = await Spot.findOne({
+      where: { address: "3008 Tetrick Road" }
     });
 
     return queryInterface.bulkInsert('SpotImages', [
@@ -30,7 +51,42 @@ module.exports = {
         spotId: spotThree.id,
         url: 'https://a0.muscache.com/im/pictures/miso/Hosting-21605015/original/e914d980-5585-4fbe-a0ef-21b8d0b1cf8c.jpeg?im_w=1200',
         preview: true
-      }
+      },
+      {
+        spotId: spotFour.id,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-665478702415015675/original/44c4197d-2e82-47cb-a64d-04152dfd73b0.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotFive.id,
+        url: 'https://a0.muscache.com/im/pictures/8f4290bc-2128-44e1-b4f3-9c470c69dd95.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotSix.id,
+        url: 'https://a0.muscache.com/im/pictures/7ef420b6-4c3e-463f-825f-3f59e252d369.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotSeven.id,
+        url: 'https://a0.muscache.com/im/pictures/3fb69cb1-32dc-4b21-a0fd-ee3e081814ac.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotEight.id,
+        url: 'https://a0.muscache.com/im/pictures/2e24b098-41ed-4d92-80c0-1e8d65bfbce4.jpg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotNine.id,
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-49719893/original/12fcb7ad-dcf2-4721-84bf-eebd697d8be7.jpeg?im_w=1200',
+        preview: true
+      },
+      {
+        spotId: spotTen.id,
+        url: 'https://a0.muscache.com/im/pictures/63a21006-5525-4932-88bf-5d5e51d6eb9b.jpg?im_w=1200',
+        preview: true
+      },
     ]);
   },
 
@@ -38,7 +94,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('SpotImages', {
       // url: 'image url'
-      spotId: { [Op.in]: [spotOne.id, spotTwo.id, spotThree.id] }
+      spotId: { [Op.in]: [spotOne.id, spotTwo.id, spotThree.id, spotFour.id, spotFive.id, spotSix.id, spotSeven.id, spotEight.id, spotNine.id, spotTen.id] }
     }, {});
   }
 };
