7a11b07d5d949b2d1f06620b4547d877b537fae1
Max Yaswen <myaswen@gmail.com>
2022-10-17 11:19:08 -0700

    Spot body content layout styling

--- frontend/src/components/SpotBodyContent/index.js
+++ frontend/src/components/SpotBodyContent/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { TH_fetchSpots } from '../../store/spotReducer';
 import './SpotBodyContent.css';
 
 const SpotBodyContent = () => {
@@ -8,13 +9,25 @@ const SpotBodyContent = () => {
 
     const dispatch = useDispatch();
     const spotObject = useSelector(state => state.spots.allspots[spotId]);
-    console.log(spotObject);
+
+    useEffect(() => {
+        dispatch(TH_fetchSpots());
+    }, [dispatch]);
 
     return (
         <div className='spot_body_wrapper'>
-            <h2>{spotObject.name}</h2>
-            <div className='location_header'>{spotObject.city}, {spotObject.state}</div>
+            <h2>{spotObject?.name}</h2>
+            <div className='location_header'>{spotObject?.city}, {spotObject?.state}</div>
             <div className='images_grid'>
+            </div>
+            <div className='subcontent_wrapper'>
+                <div className='othercontent_wrapper'>
+                    <div className='spot_description'>{spotObject?.description}</div>
+
+                </div>
+                <div className='booking_wrapper'>
+                    <div className='booking_card_STANDIN' />
+                </div>
 
             </div>
         </div>
