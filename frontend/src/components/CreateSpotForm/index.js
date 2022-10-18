1e4565152e9771fe51588d7adf8919c78b8e10c8
Max Yaswen <myaswen@gmail.com>
2022-10-17 18:47:39 -0700

    Add create spot redux

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { TH_postSpot } from "../../store/spotReducer";
 import './CreateSpotForm.css';
 
 const CreateSpotForm = () => {
+    const dispatch = useDispatch();
     const [address, setAddress] = useState("");
     const [city, setCity] = useState("");
     const [state, setState] = useState("");
@@ -22,7 +24,7 @@ const CreateSpotForm = () => {
         e.preventDefault();
         setErrors([]);
 
-        console.log({
+        const inputData = {
             address,
             city,
             state,
@@ -32,86 +34,94 @@ const CreateSpotForm = () => {
             name,
             price,
             description
-        })
+        }
+
+        dispatch(TH_postSpot(inputData))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(Object.values(data.errors));
+            });
+
+        // TO DO: redirect user or reset form inputs if success
     }
 
     return (
         <div className="create_spot_wrapper">
             <div className='create_spot_form_wrapper'>
-            <h2>Create a spot</h2>
-            <form onSubmit={handleSubmit}>
-                <div className='error_list'>
-                    {/* {errors.map((error, idx) => <div key={idx}>{error}</div>)} */}
-                </div>
-                <div className='input_wrapper'>
-                    <input
-                        placeholder='Address'
-                        type="text"
-                        required
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
-                    <input
-                        placeholder='City'
-                        type="text"
-                        required
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    />
-                    <input
-                        placeholder='State'
-                        type="text"
-                        required
-                        value={state}
-                        onChange={(e) => setState(e.target.value)}
-                    />
-                    <input
-                        placeholder='Country'
-                        type="text"
-                        required
-                        value={country}
-                        onChange={(e) => setCountry(e.target.value)}
-                    />
-                    <input
-                        placeholder='Latitude'
-                        type="number"
-                        required
-                        value={lat}
-                        onChange={(e) => setLat(e.target.value)}
-                    />
-                    <input
-                        placeholder='Longitude'
-                        type="number"
-                        required
-                        value={lng}
-                        onChange={(e) => setLng(e.target.value)}
-                    />
-                    <input
-                        placeholder='Give your spot a name'
-                        type="text"
-                        required
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <input
-                        placeholder='Price per night'
-                        type="number"
-                        required
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-                    <textarea
-                    placeholder="Enter a description"
-                    // cols="30"
-                    rows="16"
-                    required
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-                <button className="submit_form_button" type="submit">Create</button>
-            </form>
-        </div>
+                <h2>Create a spot</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className='error_list'>
+                        {errors.map((error, idx) => <div key={idx}>{error}</div>)}
+                    </div>
+                    <div className='input_wrapper'>
+                        <input
+                            placeholder='Address'
+                            type="text"
+                            required
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                        <input
+                            placeholder='City'
+                            type="text"
+                            required
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                        <input
+                            placeholder='State'
+                            type="text"
+                            required
+                            value={state}
+                            onChange={(e) => setState(e.target.value)}
+                        />
+                        <input
+                            placeholder='Country'
+                            type="text"
+                            required
+                            value={country}
+                            onChange={(e) => setCountry(e.target.value)}
+                        />
+                        <input
+                            placeholder='Latitude'
+                            type="number"
+                            required
+                            value={lat}
+                            onChange={(e) => setLat(e.target.value)}
+                        />
+                        <input
+                            placeholder='Longitude'
+                            type="number"
+                            required
+                            value={lng}
+                            onChange={(e) => setLng(e.target.value)}
+                        />
+                        <input
+                            placeholder='Give your spot a name'
+                            type="text"
+                            required
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            placeholder='Price per night'
+                            type="number"
+                            required
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+                        <textarea
+                            placeholder="Enter a description"
+                            // cols="30"
+                            rows="16"
+                            required
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </div>
+                    <button className="submit_form_button" type="submit">Create</button>
+                </form>
+            </div>
         </div>
     );
 }
--- frontend/src/store/spotReducer.js
+++ frontend/src/store/spotReducer.js
@@ -1,5 +1,8 @@
+import { csrfFetch } from "./csrf";
+
 const LOAD_SPOTS = 'spots/loadSpots';
 const LOAD_SPOT = 'spots/loadSpot';
+const CREATE_SPOT = 'spots/createSpot';
 
 export const AC_loadSpots = (spots) => {
     return {
@@ -11,6 +14,13 @@ export const AC_loadSpots = (spots) => {
 export const AC_loadSpot = (spot) => {
     return {
         type: LOAD_SPOT,
+        payload: spot
+    }
+}
+
+export const AC_createSpot = (spot) => {
+    return {
+        type: CREATE_SPOT,
         payload: spot
     }
 }
@@ -27,6 +37,32 @@ export const TH_fetchSpot = (spotId) => async (dispatch) => {
     dispatch(AC_loadSpot(spot));
 }
 
+
+export const TH_postSpot = (userInput) => async (dispatch) => {
+    const responseOne = await csrfFetch('/api/spots', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userInput)
+    });
+
+    if (responseOne.ok) {
+        const unformattedSpot = await responseOne.json();
+
+        console.log("NOT FORMATTED SPOT:", unformattedSpot);
+
+        const responseTwo = await fetch('/api/spots?');
+        const spotsObject = await responseTwo.json();
+
+        console.log("spotsObjectREDUX", spotsObject);
+
+        const formattedSpot = spotsObject.Spots.find(spot => spot.id === unformattedSpot.id);
+
+        console.log("FORMATTED SPOT:", formattedSpot);
+        dispatch(AC_createSpot(formattedSpot));
+        return formattedSpot;
+    }
+}
+
 const initialState = { allspots: {}, singleSpot: {} };
 
 const spotsReducer = (state = initialState, action) => {
@@ -36,7 +72,11 @@ const spotsReducer = (state = initialState, action) => {
             newState = { ...state, allspots: normalizeData([...action.payload.Spots]) };
             return newState;
         case LOAD_SPOT:
-            newState = {...state, singleSpot: {...action.payload}};
+            newState = { ...state, singleSpot: { ...action.payload } };
+            return newState;
+        case CREATE_SPOT:
+            newState = { ...state, allspots: { ...state.allspots } };
+            newState.allspots[action.payload.id] = action.payload;
             return newState;
         default:
             return state;
