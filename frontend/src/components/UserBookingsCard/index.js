aeed028e61502f0b2091a1bff2ede4ba6087daf4
Max Yaswen <myaswen@gmail.com>
2022-10-19 15:41:36 -0700

    Add delete booking option to booking booking card

--- frontend/src/components/UserBookingsCard/index.js
+++ frontend/src/components/UserBookingsCard/index.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './UserBookingsCard.css';
 
 const UserBookingsCard = ({ booking }) => {
+    const [deleteConfirm, setDeleteConfirm] = useState();
+
+    const handleDelete = async () => {
+        if (deleteConfirm) {
+            // // REFACTOR FOR DELETE BOOKING:
+            // let deletionRes = await dispatch(TH_deleteSpot(spotId))
+            //     .catch(async (res) => {
+            //         const data = await res.json();
+            //         if (data && data.errors) setErrors(Object.values(data.errors));
+            //     });
+
+            // if (deletionRes) history.push('/');
+            console.log("SO IT SHALL BE DONE");
+        } else {
+            setDeleteConfirm(true);
+        }
+    }
+
     return (
         <div className="user_bookings_card">
             <div className="user_booking_info">
@@ -25,6 +43,14 @@ const UserBookingsCard = ({ booking }) => {
                         <div>{booking.Spot.city}, {booking.Spot.state}</div>
                         <div className="res_country">{booking.Spot.country}</div>
                     </div>
+                </div>
+                <div className="delete_booking_container">
+                    {deleteConfirm && (
+                        <div className="delete_booking_warning">
+                            Are you sure? Click again to proceed:
+                        </div>
+                    )}
+                    <button onClick={handleDelete} className="delete_booking_button">Cancel</button>
                 </div>
             </div>
             <div className="booking_spot_preview">
